b1299940e6ab0cfcd2aad8b2d1701885208e5a46
Kun Vath <matrixxteame@gmail.com>
2025-07-31 22:17:41 +0700

    remove venv and restructure db conn

--- stationery_proj/stationery_proj/settings.py
+++ stationery_proj/stationery_proj/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+from .database_conn import DATABASES
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -86,6 +86,7 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+
 
 
 #postgresql
